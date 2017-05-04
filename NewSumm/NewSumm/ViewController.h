eb113aba24fc15ff09c79dda390e1e5bc9950dcf
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-04 19:09:40 +0100

    Initial Xcode project

---
+++ NewSumm/NewSumm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewSumm
+//
+//  Created by Kunal Wagle on 04/05/2017.
+//  Copyright © 2017 Kunal Wagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
