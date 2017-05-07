a6f7095a70e40b34d4174c704d536c2adb89b988
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-07 12:43:34 +0100

    Created large panel

---
+++ NewSumm/NewSumm/LargePanelCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  LargePanelCollectionViewCell.h
+//  NewSumm
+//
+//  Created by Kunal Wagle on 07/05/2017.
+//  Copyright © 2017 Kunal Wagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LargePanelCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UITextView *text;
+
+@end
---
+++ NewSumm/NewSumm/LargePanelCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  LargePanelCollectionViewCell.m
+//  NewSumm
+//
+//  Created by Kunal Wagle on 07/05/2017.
+//  Copyright © 2017 Kunal Wagle. All rights reserved.
+//
+
+#import "LargePanelCollectionViewCell.h"
+
+@implementation LargePanelCollectionViewCell
+
+@end
