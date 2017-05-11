a2bc5b2c7d42c257686552f23709a54e4f8dc80f
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-11 11:06:47 +0100

    Added toggle buttons for iPhone

--- NewSumm/NewSumm/PhoneArticleViewController.m
+++ NewSumm/NewSumm/PhoneArticleViewController.m
@@ -27,14 +27,7 @@
     for (NSDictionary *dictionary in article.articles) {
         [sources addObject:[dictionary objectForKey:@"source"]];
     }
-    [sources sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
-    NSString *sourceString = [NSString stringWithFormat:@"[%@]", [sources componentsJoinedByString:@","]];
-    NSArray *text = [[article summaryMap] objectForKey:sourceString];
-    self.navigationItem.title = @"";
-    articleText = [[NSMutableArray alloc] init];
-    for (NSDictionary *dictionary in text) {
-        [articleText addObject:dictionary[@"sentence"]];
-    }
+    [self updateSources];
     tv.estimatedRowHeight = 260;
     tv.rowHeight = UITableViewAutomaticDimension;
 }
@@ -92,12 +85,24 @@
         case 3: {
             cell = [tableView dequeueReusableCellWithIdentifier:@"source" forIndexPath:indexPath];
             NSDictionary *dictionary = [[article articles] objectAtIndex:[indexPath row]];
+            NSString *source = dictionary[@"source"];
             UILabel *textLabel = [cell viewWithTag:103];
-            UILabel *detailTextLabel = [cell viewWithTag:104];
             UIImageView *imageView = [cell viewWithTag:102];
+            UILabel *detailTextLabel = [cell viewWithTag:104];
+            UISwitch *toggle = [cell viewWithTag:105];
+            detailTextLabel.text = @"Click to view article";
+            BOOL on = [sources indexOfObject:source] != NSNotFound;
+            if (on && [sources count] == 1) {
+                toggle.enabled = NO;
+                [toggle removeTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
+            } else {
+                toggle.enabled = YES;
+                [toggle addTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
+            }
+            toggle.on = on;
+            toggle.tag = 105 + [indexPath row];
             textLabel.text = dictionary[@"title"];
-            detailTextLabel.text = dictionary[@"articleUrl"];
-            UIImage *image = [UIImage imageNamed:dictionary[@"source"]];
+            UIImage *image = [UIImage imageNamed:source];
             imageView.image = image;
             imageView.layer.cornerRadius = imageView.frame.size.width / 2;
             imageView.layer.masksToBounds = YES;
@@ -117,6 +122,31 @@
     } else {
         return @"";
     }
+}
+
+-(void)switchChanged:(UISwitch*)toggle {
+    NSInteger rowChanged = toggle.tag - 105;
+    NSString *source = [[article.articles objectAtIndex:rowChanged] objectForKey:@"source"];
+    if (toggle.isOn) {
+        if ([sources indexOfObject:source] == NSNotFound) {
+            [sources addObject:source];
+        }
+    } else {
+        [sources removeObject:source];
+    }
+    [self updateSources];
+}
+
+-(void)updateSources {
+    [sources sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    NSString *sourceString = [NSString stringWithFormat:@"[%@]", [sources componentsJoinedByString:@","]];
+    NSArray *text = [[article summaryMap] objectForKey:sourceString];
+    self.navigationItem.title = @"";
+    articleText = [[NSMutableArray alloc] init];
+    for (NSDictionary *dictionary in text) {
+        [articleText addObject:dictionary[@"sentence"]];
+    }
+    [tv reloadData];
 }
 
 
