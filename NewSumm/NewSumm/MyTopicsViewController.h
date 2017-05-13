4a6baa4713145e90c4c4cc663fbb142f7a8b1764
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-13 15:12:02 +0100

    Created Login view controllers

---
+++ NewSumm/NewSumm/MyTopicsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTopicsViewController.h
+//  NewSumm
+//
+//  Created by Kunal Wagle on 13/05/2017.
+//  Copyright © 2017 Kunal Wagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTopicsViewController : UIViewController
+
+@end
---
+++ NewSumm/NewSumm/MyTopicsViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyTopicsViewController.m
+//  NewSumm
+//
+//  Created by Kunal Wagle on 13/05/2017.
+//  Copyright © 2017 Kunal Wagle. All rights reserved.
+//
+
+#import "MyTopicsViewController.h"
+
+@interface MyTopicsViewController ()
+
+@end
+
+@implementation MyTopicsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
