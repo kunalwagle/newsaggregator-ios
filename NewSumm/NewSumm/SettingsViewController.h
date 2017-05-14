20108a1547a9b20ec170f3f6a3475e755066c89c
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-14 15:41:48 +0100

    Settings view controller

---
+++ NewSumm/NewSumm/SettingsViewController.h
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.h
+//  NewSumm
+//
+//  Created by Kunal Wagle on 14/05/2017.
+//  Copyright © 2017 Kunal Wagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MyTopicsViewController.h"
+
+@interface SettingsViewController : UIViewController<LoginDelegate, UITableViewDelegate, UITableViewDataSource>
+
+@property NSArray *topics;
+@property (weak, nonatomic) IBOutlet UITableView *tv;
+@property (weak, nonatomic) IBOutlet UILabel *loginLabel;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+- (IBAction)login:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *logout;
+- (IBAction)logout:(id)sender;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@end
---
+++ NewSumm/NewSumm/SettingsViewController.m
@@ -0,0 +1,110 @@
+//
+//  SettingsViewController.m
+//  NewSumm
+//
+//  Created by Kunal Wagle on 14/05/2017.
+//  Copyright © 2017 Kunal Wagle. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+#import "Login.h"
+#import "LoginViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+@synthesize topics;
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self loggedIn];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)loggedIn {
+    BOOL loggedIn = [[NSUserDefaults standardUserDefaults] boolForKey:@"loggedIn"];
+    if (loggedIn) {
+        [self setLoginItemsHidden];
+        NSString *emailAddress = [[NSUserDefaults standardUserDefaults] objectForKey:@"emailAddress"];
+        [_activityIndicator startAnimating];
+        [_activityIndicator setHidden:NO];
+        [Login login:emailAddress withHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (!error) {
+                NSError *jsonError;
+                NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NULL error:&jsonError];
+                topics = [dict objectForKey:@"topics"];
+                dispatch_sync(dispatch_get_main_queue(), ^{
+                    [_tv reloadData];
+                    [self setLoginItemsHidden];
+                });
+            } else {
+                NSLog(@"Error: %@", error);
+            }
+        }];
+    } else {
+        [self showLoginItems];
+    }
+}
+
+-(void)setLoginItemsHidden {
+    [_loginLabel setHidden:YES];
+    [_loginButton setHidden:YES];
+    [_logout setHidden:NO];
+    [_activityIndicator setHidden:YES];
+    [_tv setHidden:NO];
+}
+
+-(void)showLoginItems {
+    [_loginLabel setHidden:NO];
+    [_loginButton setHidden:NO];
+    [_logout setHidden:YES];
+    [_activityIndicator setHidden:YES];
+    [_tv setHidden:YES];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [topics count];
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    UILabel *label = [cell viewWithTag:101];
+    label.text = [[topics objectAtIndex:[indexPath row]] objectForKey:@"label"];
+    return cell;
+}
+
+-(NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"Select a topic to alter settings or unsubscribe";
+}
+
+- (IBAction)login:(id)sender {
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"login"];;
+    [loginViewController setDelegate:self];
+    [self presentViewController:loginViewController animated:YES completion:nil];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)logout:(id)sender {
+}
+@end
