271bec2dced2f1e5aba2af5f46b4d76c216fccb5
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-16 11:31:18 +0100

    Colours fixed

--- NewSumm/NewSumm/UtilityMethods.m
+++ NewSumm/NewSumm/UtilityMethods.m
@@ -31,51 +31,51 @@
 +(UIColor*)getColour:(NSString*)source {
     
     if ([source isEqualToString:@"the-guardian-uk"]) {
-        return [UIColor colorWithRed:0 green:128 blue:128 alpha:0.4];
+        return [UIColor colorWithRed:0 green:128.0/255.0 blue:128.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"independent"]) {
-        return [UIColor colorWithRed:255 green:255 blue:0 alpha:0.4];
+        return [UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"associated-press"]) {
-        return [UIColor colorWithRed:144 green:238 blue:144 alpha:0.4];
+        return [UIColor colorWithRed:144.0/255.0 green:238.0/255.0 blue:144.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"reuters"]) {
-        return [UIColor colorWithRed:165 green:42 blue:42 alpha:0.4];
+        return [UIColor colorWithRed:165.0/255.0 green:42.0/255.0 blue:42.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"business-insider-uk"]) {
-        return [UIColor colorWithRed:0 green:128 blue:0 alpha:0.4];
+        return [UIColor colorWithRed:0.0/255.0 green:128.0/255.0 blue:0.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"daily-mail"]) {
-        return [UIColor colorWithRed:128 green:0 blue:128 alpha:0.4];
+        return [UIColor colorWithRed:128.0/255.0 green:0 blue:128.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"espn-cric-info"]) {
-        return [UIColor colorWithRed:173 green:216 blue:230 alpha:0.4];
+        return [UIColor colorWithRed:173.0/255.0 green:216.0/255.0 blue:230.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"metro"]) {
-        return [UIColor colorWithRed:255 green:215 blue:0 alpha:0.4];
+        return [UIColor colorWithRed:255.0/255.0 green:215.0/255.0 blue:0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"mirror"]) {
-        return [UIColor colorWithRed:255 green:0 blue:0 alpha:0.4];
+        return [UIColor colorWithRed:255.0/255.0 green:0 blue:0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"newsweek"]) {
-        return [UIColor colorWithRed:255 green:0 blue:255 alpha:0.4];
+        return [UIColor colorWithRed:255.0/255.0 green:0 blue:255.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"the-telegraph"]) {
-        return [UIColor colorWithRed:0 green:0 blue:139 alpha:0.4];
+        return [UIColor colorWithRed:0 green:0 blue:139.0/255.0 alpha:0.4];
     }
     
     if ([source isEqualToString:@"the-times-of-india"]) {
-        return [UIColor colorWithRed:255 green:165 blue:0 alpha:0.4];
+        return [UIColor colorWithRed:255.0/255.0 green:165.0/255.0 blue:0 alpha:0.4];
     }
     
     return NULL;
